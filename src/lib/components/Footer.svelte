db800a4736779282f73e62aaa0f1471f0ac621c8
Mustafa Hussaini <mustafa.hussaini@etomer.com>
2023-02-05 13:42:38 +0100

    added tailwind to the project. recreating our status-qup

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,1 @@
+<footer>something footer</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,21 @@
+<header class="header flex align-items-center justify-content-sb">
+	<a href="index.html">
+		<img
+			class="main-logo"
+			width="150"
+			height="150"
+			src="../assets/logo.png"
+			alt="Das Logo von global-getaways"
+		/>
+	</a>
+	<nav class="nav">
+		<ul class="none-bullet flex">
+			<li>
+				<a href="blog.html">Blogbeiträge</a>
+			</li>
+			<li>
+				<a href="about.html">Über uns</a>
+			</li>
+		</ul>
+	</nav>
+</header>
