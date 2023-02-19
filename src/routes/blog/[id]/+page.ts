e377c848b00316d6f97efea4fd5d20aaf4a282ef
Mustafa Hussaini <mustafa.hussaini@etomer.com>
2023-02-19 21:31:45 +0100

    some more bugs removed

--- src/routes/blog/[id]/+page.ts
+++ src/routes/blog/[id]/+page.ts
@@ -2,24 +2,38 @@ import type { PageLoad } from "./$types";
 import type { Blog, Comment, Favorite } from "$lib/types/models";
 
 export const load = (async ({ params, fetch }) => {
-	const res = await fetch(`http://172.29.45.205:8080/api/blog/single/${params.id}`, {
+	const blog: Blog = await fetch(`http://172.29.45.205:8080/api/blog/single/${params.id}`, {
 		credentials: "include",
 		method: "GET"
-	});
+	})
+		.then((res) => res.json())
+		.catch((err) => {
+			console.error(err);
+		});
 
-	const resComment = await fetch(`http://172.29.45.205:8080/api/comment/list/${params.id}`, {
-		credentials: "include",
-		method: "GET"
-	});
+	const comments: Comment[] = await fetch(
+		`http://172.29.45.205:8080/api/comment/list/${params.id}`,
+		{
+			credentials: "include",
+			method: "GET"
+		}
+	)
+		.then((res) => res.json())
+		.catch((err) => {
+			console.error(err);
+		});
 
-	const resFavorite = await fetch(`http://172.29.45.205:8080/api/favorite/single/${params.id}`, {
-		credentials: "include",
-		method: "GET"
-	});
+	const favorite: Favorite = await fetch(
+		`http://172.29.45.205:8080/api/favorite/single/${params.id}`,
+		{
+			credentials: "include",
+			method: "GET"
+		}
+	)
+		.then((res) => res.json())
+		.catch((err) => {
+			console.error(err);
+		});
 
-	const comments: Comment[] = await resComment.json();
-	const favorite: Favorite = await resFavorite.json();
-
-	const blog: Blog = await res.json();
 	return { blog, comments, favorite };
 }) satisfies PageLoad;
